594c6440f00ec882f632fa36af7c127797cad053
adlens <adlens@hotmail.com>
2024-03-01 17:46:20 +0000

    Store stock URLs based on search queries in a JSON file to prevent redundant searches

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import requests
+import json
 from bs4 import BeautifulSoup
 from flask import Flask, request, jsonify, url_for
 from flask_caching import Cache
@@ -14,6 +15,20 @@ logging.basicConfig(level=logging.INFO)
 
 session = requests.Session()
 
+
+def load_search_results():
+    # Load saved seaching url results from JSON file
+    if os.path.exists(search_results_file):
+        with open(search_results_file, 'r') as file:
+            return json.load(file)
+    else:
+        return {}
+
+def save_search_results():
+    # Update the url results in JSON
+    with open(search_results_file, 'w') as file:
+        json.dump(search_results, file)
+
 def safe_request(url):
     try:
         response = session.get(url)
@@ -23,8 +38,14 @@ def safe_request(url):
         logging.error(f"Request failed: {e}")
         return None
 
-@cache.memoize(timeout=50)  # 缓存时间为50秒
+search_results_file = 'search_results.json'
+search_results = load_search_results()
+
 def find_urls(search_input):
+    global search_results
+    # If search_input is already recorded, directly return the results
+    if search_input in search_results:  
+        return search_results[search_input]
     base_url = os.getenv("BASE_URL", "https://www.hl.co.uk/shares/search-for-investments")
     search_url = f"{base_url}?stock_search_input={search_input}"
     response = safe_request(search_url)
@@ -38,6 +59,8 @@ def find_urls(search_input):
                 link_element = stock.find('td').find('a', href=True)
                 if link_element:
                     urls.append(link_element['href'])
+    search_results[search_input] = urls
+    save_search_results() 
     return urls
 
 def get_stock_info(stock_url):
