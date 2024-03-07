52388aae636db426beeeeb546176c5d46f538e2e
adlens <adlens@hotmail.com>
2024-03-07 19:58:25 +0000

    Implement search count tracking and auto-prune for JSON storage

--- scraper.py
+++ scraper.py
@@ -15,6 +15,14 @@ logging.basicConfig(level=logging.INFO)
 
 session = requests.Session()
 
+def safe_request(url):
+    try:
+        response = session.get(url)
+        response.raise_for_status()
+        return response
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Request failed: {e}")
+        return None
 
 def load_search_results():
     # Load saved seaching url results from JSON file
@@ -29,14 +37,26 @@ def save_search_results():
     with open(search_results_file, 'w') as file:
         json.dump(search_results, file)
 
-def safe_request(url):
-    try:
-        response = session.get(url)
-        response.raise_for_status()
-        return response
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Request failed: {e}")
-        return None
+
+def update_search_results(search_input, urls):
+    # Add or update search results and maintain size limit
+    global search_results
+    dict_size = 100
+    # Increment count if exists, else add new entry with count 1
+    if search_input in search_results:
+        search_results[search_input]['urls'] = urls
+        search_results[search_input]['count'] += 1
+    else:
+        search_results[search_input] = {'urls': urls, 'count': 1}
+    
+    # Check size and remove the least frequently used if necessary
+    
+    if len(search_results) > dict_size:
+        # Find the search input with the minimum count
+        least_used = min(search_results.keys(), key=lambda k: search_results[k]['count'])
+        del search_results[least_used]
+    save_search_results()
+
 
 search_results_file = 'search_results.json'
 search_results = load_search_results()
@@ -45,7 +65,9 @@ def find_urls(search_input):
     global search_results
     # If search_input is already recorded, directly return the results
     if search_input in search_results:  
-        return search_results[search_input]
+        search_results[search_input]['count'] += 1  # Update count every time it's accessed
+        save_search_results()
+        return search_results[search_input]['urls']
     base_url = os.getenv("BASE_URL", "https://www.hl.co.uk/shares/search-for-investments")
     search_url = f"{base_url}?stock_search_input={search_input}"
     response = safe_request(search_url)
@@ -59,8 +81,8 @@ def find_urls(search_input):
                 link_element = stock.find('td').find('a', href=True)
                 if link_element:
                     urls.append(link_element['href'])
-    search_results[search_input] = urls
-    save_search_results() 
+    if urls:
+        update_search_results(search_input, urls)
     return urls
 
 def get_stock_info(stock_url):
