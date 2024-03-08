4757f83ef9a78315bec1f6fabf9f6a7b09a112aa
adlens <adlens@hotmail.com>
2024-03-08 15:58:02 +0000

    Improve efficiency in fetching stock information from multiple URLs by using ThreadPoolExecutor for concurrent requests and response handling.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+from flask import Flask, jsonify
+from scraper import find_urls, get_stock_info, load_search_results
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import logging
 import os
-from flask import Flask, request, jsonify
 from flask_caching import Cache
-from scraper import find_urls, get_stock_info, load_search_results
 
 app = Flask(__name__)
 app.config['CACHE_TYPE'] = 'redis'
@@ -16,18 +17,27 @@ search_results_file = 'search_results.json'
 search_results = load_search_results(search_results_file)
 
 @app.route('/get_stock_info/<search_input>', methods=['GET'])
-@cache.memoize(timeout=300)  # Cache data for 5 minutes
+@cache.memoize(timeout=300)
 def api_get_stock_info(search_input):
     if not search_input:
+        logging.error("Missing search input")
         return jsonify({'error': 'Missing search input'}), 400
     try:
-        results = {}
         base_url = os.getenv("BASE_URL", "https://www.hl.co.uk/shares/search-for-investments")
         urls = find_urls(search_input.lower(), base_url, search_results, search_results_file)
-        for url in urls:
-            stock_name, stock_info = get_stock_info(url)
-            if stock_name:
-                results[stock_name] = stock_info
+        
+        results = {}
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            future_to_url = {executor.submit(get_stock_info, url): url for url in urls}
+            for future in as_completed(future_to_url):
+                url = future_to_url[future]
+                try:
+                    stock_name, stock_info = future.result()
+                    if stock_name:
+                        results[stock_name] = stock_info
+                except Exception as exc:
+                    logging.error(f'{url} generated an exception: {exc}')
+        
         return jsonify(results), 200
     except Exception as e:
         logging.error(f"Error processing request: {e}")
